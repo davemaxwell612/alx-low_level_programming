44b93508a43f4bda10a046204d5de48dd31bc375
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-09 05:36:19 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - entry point
+ * My hello.printf task
+ * Return:return 1 after program execution
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
