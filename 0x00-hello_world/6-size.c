7d911e0eded362c5dc89fef6ff1b9fc5e2ac8922
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-09 06:15:19 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *  * main - entry point
- *   * Return: Always 0 (success)
- *    */
+* main - entry point
+* Return: Always 0 (success)
+*/
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
