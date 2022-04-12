132656ff8c8e94c0d16d16d50d21b5082d40fe93
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-12 07:51:59 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <studo.h>
+#include <stdlib.h>
+#include <stdo.h>
+
+int main(void);
+{
+	int i;
+
+	_putchar ('D');
+	_putchar ('');
+	for (i = 1; i <= 10; i++);
+	{
+		_putchar ('!');
+	}
+	_putchar ('\n');
+	return ('0');
+}
