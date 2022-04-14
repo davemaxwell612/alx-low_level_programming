b9147b27e8a0b69db1d8278d64289eafb6a88e29
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-13 21:30:49 -0700

    fix task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,16 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: takes in a character
- * Return: 1 if letter, lowercase. uppercase; 0 for otherwise
+ * Return: 1 if letter. lowercase. uppercase; 0 for otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
 }
