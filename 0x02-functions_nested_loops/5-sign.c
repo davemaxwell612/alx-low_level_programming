4346e5dda8a3f4f7a9382a57c8eac26b61f763a3
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-13 21:28:06 -0700

    fix task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: takes in a character
- * Return: 1 if letter. lowercase. uppercase; 0 for otherwise
+ * Return: 1 if letter, lowercase. uppercase; 0 for otherwise
  */
 int _isalpha(int c)
 {
